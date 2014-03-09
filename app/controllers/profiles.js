91f6c0951a4cac200ad837ac63a69c4d21b528bb
Siedrix <Siedrix@gmail.com>
2014-03-09 02:41:44 -0600

    Added bio for users

--- app/controllers/profiles.js
+++ app/controllers/profiles.js
@@ -1,5 +1,6 @@
 var controller = require('stackers'),
-	_ =  require('underscore');
+	_ =  require('underscore'),
+	marked = require('marked');
 
 var Users   = require('../collections/users');
 var Tickets = require('../collections/tickets');
@@ -43,13 +44,34 @@ profileController.get('/:userName', function (req, res) {
 
 		return events.fetchFilter(function (item) {return eventsAssisted.indexOf(item.slug) >= 0; });
 	}).then(function () {
+		var updatedBio = req.query['update-bio'] ? true : false;
+		var bioAsHtml  = marked(user.get('bio'));
+
+		console.log('bioAsHtml?', bioAsHtml);
+
 		res.render('profiles/profile', {
 			currentUser  : user.toJSON(),
 			events       : events.toJSON(),
-			profileOwner : profileOwner
+			profileOwner : profileOwner,
+			updatedBio   : updatedBio,
+			bioAsHtml    : bioAsHtml
 		});
 	}).catch(function (err) {
 		res.send(500, err);
+	});
+});
+
+profileController.post('/:userName/update-bio', function (req, res) {
+	var user = res.data.userName;
+
+	if( _.isEmpty(res.data.userName) ){return res.sendError(404, 'user not found');}
+	if( !(req.session.passport && req.session.passport.user && req.session.passport.user.username === user.get('username')) ){return res.sendError(403, 'forbiden');}
+
+	user.set('bio', req.body.bio);
+	user.save().then(function () {
+		res.redirect( 'perfil/' + user.get('username') + '?update-bio=true' );
+	}).catch(function (err) {
+		res.sendError(500, err);
 	});
 });
 
