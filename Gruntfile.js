9d543d388a02539a65d32b9cd8a7a9cd421fed6f
Jeduan Cornejo <jeduan@gmail.com>
2014-03-29 16:08:40 -0600

    Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,49 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    stylus: {
-      dev: {
-        files: {
-          'public/css/main.css': 'public/stylus/main.styl',
-          'public/css/admin.css': 'public/stylus/admin.styl',
-        }
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      stylus: {
-        files: ['public/stylus/**/*.styl'],
-        tasks: ['stylus:dev']
-      }
-    }
-  })
-  grunt.loadNpmTasks('grunt-contrib-stylus')
-  grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.initConfig({
+		stylus: {
+			dev: {
+				files: {
+					'public/css/main.css': 'public/stylus/main.styl',
+					'public/css/admin.css': 'public/stylus/admin.styl',
+				}
+			}
+		},
 
-  grunt.registerTask('styles', ['stylus:dev', 'watch'])
-}
+		concat: {
+			dev: {
+				files: {
+					'public/js/vendors.js': [
+						'bower_components/jquery/dist/jquery.js',
+						'bower_components/bootstrap/dist/js/bootstrap.js',
+					],
+					'public/css/vendors.css': [
+						'bower_components/bootstrap/dist/css/bootstrap.css',
+						'bower_components/font-awesome/css/font-awesome.css'
+					],
+				}
+			}
+		},
+
+		uglify: {
+			dev: {
+				files: {}
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			stylus: {
+				files: ['public/stylus/**/*.styl'],
+				tasks: ['stylus:dev']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('styles', ['stylus:dev', 'watch']);
+};
